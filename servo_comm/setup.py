dfb9e143b56b84eb2ef46887f64651cef385897d
Michał Wójcik <wojcikmichal98@gmail.com>
2019-07-31 19:22:23 +0200

    Change directory name because ROS2

--- servo_comm/setup.py
+++ servo_comm/setup.py
@@ -27,7 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'listener = src.listener:main',
+            'listener = servo_comm.listener:main',
         ],
     },
 )
