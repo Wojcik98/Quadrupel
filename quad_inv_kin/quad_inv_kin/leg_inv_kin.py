887efcb802424b9de14e810cfc638b5e9c27d37b
Michał Wójcik <wojcikmichal98@gmail.com>
2019-09-25 19:11:51 +0200

    Use global parameters in inverse kinematics

--- quad_inv_kin/quad_inv_kin/leg_inv_kin.py
+++ quad_inv_kin/quad_inv_kin/leg_inv_kin.py
@@ -1,15 +1,20 @@
 from math import atan2, acos, sqrt, pi
+import json
+import os
 
 import rclpy
 from rclpy.node import Node
+from ament_index_python.packages import get_package_share_directory
 
 from custom.srv import LegInvKin
 
 
 class LegInvKinServer(Node):
-
     def __init__(self):
         super().__init__('leg_inv_kin_server')
+
+        self.leg_desc = self.get_leg_desc()
+
         self.srv = self.create_service(
             LegInvKin, 'leg_inv_kin', self.leg_inv_kin_callback
         )
@@ -32,20 +37,30 @@ class LegInvKinServer(Node):
 
         return response
 
-    def calc(self, x, y, z):   # TODO z, read from dh.json
-        alpha = atan2(y, x)
-        xy = sqrt(x**2 + y**2)
-        a = 0.066
-        b = 0.052
-        r = sqrt(xy**2 + z**2)
+    def calc(self, x, y, z):
+        alpha = atan2(y, x) # TODO use d
+        w = sqrt(x**2 + y**2) - self.leg_desc[1]['a']
+        z = z - self.leg_desc[1]['d']
+        a = self.leg_desc[2]['a']
+        b = self.leg_desc[3]['a']
+        r = sqrt(w**2 + z**2)
         delta = acos((a**2 + b**2 - r**2) / (2 * a * b))
         gamma = pi - delta
 
-        b1 = atan2(xy, -z)
+        b1 = atan2(w, -z)
         b2 = acos((a**2 + r**2 - b**2) / (2 * a * r))
         beta = b1 + b2
 
         return float(alpha), float(beta - pi/2), float(-gamma)
+
+    def get_leg_desc(self):
+        params_path = os.path.join(
+            get_package_share_directory('quad_params'),
+            'quad_params.json'
+        )
+        with open(params_path, 'r') as file:
+            legs_params = json.load(file)
+        return legs_params['general']
 
 
 def main(args=None):
--- quad_steer/quad_steer/joy_steer.py
+++ quad_steer/quad_steer/joy_steer.py
@@ -36,7 +36,7 @@ class JoySteer(Node):
         up = msg.buttons[5] - msg.buttons[7]
         self.transform.transform.translation.x = msg.axes[3] * 0.02
         self.transform.transform.translation.y = msg.axes[2] * 0.03
-        self.transform.transform.translation.z = 0.07 + up * 0.03
+        self.transform.transform.translation.z = 0.1 + up * 0.03
         self.transform.transform.rotation.x = -msg.axes[0] * 0.1
         self.transform.transform.rotation.y = msg.axes[1] * 0.1
         msg = TFMessage(transforms=[self.transform])
